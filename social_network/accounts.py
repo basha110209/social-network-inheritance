ea53490720a5216be46f5b6eaac1d5eef98f6d43
basha110209 <bashayr1029@gmail.com>
2024-11-24 07:23:49 +0300

    Update accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
-
+from datetime import datetime
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
+        post.user = self
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for user in self.following:
+            timeline.extend(user.posts)
+        return sorted(timeline, key=lambda post: post.created_at, reverse=True)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+        
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
